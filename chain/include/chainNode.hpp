0bec03a4b58304063fa432c9463564c1f2e94ac8
NotCuteButNew <2386884119@qq.com>
2023-01-05 11:20:20 +0800

    编写完chainNode.hpp和cpp, 并编写完了对应的CMakeLists.txt

---
+++ chain/include/chainNode.hpp
@@ -0,0 +1,22 @@
+﻿#ifndef CHAINNODE_HPP
+#define CHAINNODE_HPP
+
+template <class T>
+struct chainNode
+{
+    T element;
+    chainNode<T> *next;
+
+    chainNode(const T &theElement)
+    {
+        element = theElement;
+        this->next = nullptr;
+    }
+    chainNode(const T &theElement, chainNode<T> *next)
+    {
+        element = theElement;
+        this->next = next;
+    }
+};
+
+#endif
