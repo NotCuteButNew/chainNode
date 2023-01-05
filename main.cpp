e8040dd9ea5841cf923b770835db1cdb450c008f
NotCuteButNew <2386884119@qq.com>
2023-01-05 11:26:56 +0800

    编写完成main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include "chainNode.hpp"
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main(int argc, char **argv)
+{
+    if (argc > 1)
+    {
+        int *nums = new int[argc];
+        for (int i = 1; i < argc; i++)
+        {
+            nums[i - 1] = std::stoi(argv[i]);
+        }
+        chainNode<int> *headNode = new chainNode<int>(nums[0]);
+        for (int i = 1; i < argc - 1; i++)
+        {
+            headNode = new chainNode<int>(nums[i], headNode);
+        }
+        while (headNode->next != nullptr)
+        {
+            std::cout << headNode->element << " -> ";
+            headNode = headNode->next;
+        }
+        std::cout << headNode->element;
+    }
+    return 0;
 }
