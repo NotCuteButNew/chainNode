cac2dce01c6eb566b8e520b9beb3580b02c98bcd
NotCuteButNew <2386884119@qq.com>
2023-01-08 19:57:26 +0800

    新增了linearList.hpp & cpp

---
+++ chain/include/linearList.hpp
@@ -0,0 +1,18 @@
+﻿#ifndef linearList_hpp
+#define linearList_hpp
+
+template <class T>
+class linearList
+{
+private:
+    int size;
+
+public:
+    virtual ~linearList();
+    virtual bool empty() const = 0;
+    virtual int get_size() const = 0;
+    virtual void inser() = 0;
+    virtual void erase() = 0;
+};
+
+#endif
