71a390baef519156848679b0d55ebc1187dfc601
NotCuteButNew <2386884119@qq.com>
2023-01-11 14:12:41 +0800

    在linearList.hpp 中修改了函数命名

--- chain/include/linearList.hpp
+++ chain/include/linearList.hpp
@@ -1,18 +1,18 @@
 ﻿#ifndef linearList_hpp
 #define linearList_hpp
 
+#include <ostream>
 template <class T>
 class linearList
 {
 private:
-    int size;
-
 public:
     virtual ~linearList();
     virtual bool empty() const = 0;
-    virtual int get_size() const = 0;
-    virtual void inser() = 0;
-    virtual void erase() = 0;
+    virtual void insert(int theIndex, T &theElement) = 0;
+    virtual void erase(int theIndex) = 0;
+    virtual void push_back(T &theElement) = 0;
+    virtual void output(std::ostream out) const = 0;
 };
 
 #endif
