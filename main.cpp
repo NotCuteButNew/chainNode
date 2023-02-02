93f9d84d887b04bd19ad9ec2b8a2839e88aa0c46
NotCuteButNew <2386884119@qq.com>
2023-02-02 12:31:49 +0800

    修改了main.cpp中的部分输入部分，更简洁

--- main.cpp
+++ main.cpp
@@ -8,17 +8,13 @@ int main(int argc, char **argv)
     {
         student s;
         chainList<student> list;
-        int *scores = new int[argc];
-        for (int i = 1; i < argc; i += 2)
-        {
-            scores[i / 2] = std::stoi(argv[i + 1]);
-        }
         for (int i = 1; i < argc; i += 2)
         {
             s.name = argv[i];
-            s.score = scores[i / 2];
+            s.score = std::stoi(argv[i + 1]);
             list.push_back(s);
         }
+        std::cout << list;
         s.name = "abc";
         s.score = 2;
         list.insert(1, s);
