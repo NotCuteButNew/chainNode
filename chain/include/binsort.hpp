48fd8062793fc0d0c4730a1e1ad05ab562165e37
NotCuteButNew <2386884119@qq.com>
2023-02-03 19:10:53 +0800

    编写并添加bin_sort

---
+++ chain/include/binsort.hpp
@@ -0,0 +1,19 @@
+﻿#ifndef binsort_hpp
+#define binsort_hpp
+
+#include "chainList.hpp"
+
+template <class T>
+chainList<T> bin_sort(chainList<T> &theChain, int maxScore)
+{
+    chainList<T> *bin = new chainList<T>[maxScore + 1];
+    chainNode<T> *currentNode = theChain.get_head()->next;
+    while (currentNode != nullptr)
+    {
+        bin[currentNode->element].push_back(currentNode);
+        currentNode = currentNode->next;
+    }
+    return bin;
+}
+
+#endif
