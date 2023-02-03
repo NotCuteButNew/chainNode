e5d11a6477432bee5c6b55bd3c5bc4b21763e787
NotCuteButNew <2386884119@qq.com>
2023-02-03 19:10:16 +0800

    在结构体student中添加了!= 和 ->重载运算符

--- chain/include/student.hpp
+++ chain/include/student.hpp
@@ -8,6 +8,14 @@ struct student
 {
     std::string name;
     int score;
+    int operator!=(const student &x) const
+    {
+        return (score != x.score);
+    }
+    int operator->() const
+    {
+        return score;
+    }
 };
 
 std::ostream &operator<<(std::ostream &out, const student &x)
